b952f3ab1111501c0dc02e89766b6777b7e6c87c
Glsme <glasses.str.man@gmail.com>
2022-07-28 16:13:35 +0900

    Feat: WebView ToolBar 구현 및 이동 동작 코드 작업

--- SeSACLectureNetworkBasic/WebViewController.swift
+++ SeSACLectureNetworkBasic/WebViewController.swift
@@ -14,16 +14,34 @@ class WebViewController: UIViewController {
     @IBOutlet weak var webView: WKWebView!
     
     var destinationURL: String = "https://www.apple.com"
-    //App Transport Scurity Settings
-    //http X 
     
+    //App Transport Scurity Settings
+    //http X
     override func viewDidLoad() {
         super.viewDidLoad()
         
         openWebPage(url: destinationURL)
         searchBar.delegate = self
     }
-
+    
+    @IBAction func goBackButtonClicked(_ sender: UIBarButtonItem) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    
+    @IBAction func reloadButtonClicked(_ sender: UIBarButtonItem) {
+        webView.reload()
+//        openWebPage(url: self.nowURL)
+    }
+    
+    @IBAction func goFowardButtonClicked(_ sender: UIBarButtonItem) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    
+    
     func openWebPage(url: String) {
         guard let url = URL(string: url) else {
             print("Invalid URL")
@@ -37,6 +55,7 @@ class WebViewController: UIViewController {
 
 extension WebViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        openWebPage(url: searchBar.text!)
+        guard let url = searchBar.text else { return }
+        openWebPage(url: url)
     }
 }
