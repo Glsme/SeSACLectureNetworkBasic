a869d7fa65b5b1b108d290ef4e57e50dc01f8e95
Glsme <glasses.str.man@gmail.com>
2022-08-03 15:50:30 +0900

    Feat: imageSearch UI 및 동작 구현

---
+++ SeSACLectureNetworkBasic/ImageSearchCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ImageSearchCollectionViewCell.swift
+//  SeSACLectureNetworkBasic
+//
+//  Created by Seokjune Hong on 2022/08/03.
+//
+
+import UIKit
+
+class ImageSearchCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var searchImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- SeSACLectureNetworkBasic/ImageSearchViewController.swift
+++ SeSACLectureNetworkBasic/ImageSearchViewController.swift
@@ -8,15 +8,26 @@
 import UIKit
 
 import Alamofire
+import Kingfisher
 import SwiftyJSON
 
 class ImageSearchViewController: UIViewController {
 
+    @IBOutlet weak var imageSearchCollectionView: UICollectionView!
+    
+    var imageList: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        imageSearchCollectionView.delegate = self
+        imageSearchCollectionView.dataSource = self
+        
+        imageSearchCollectionView.register(UINib(nibName: ImageSearchCollectionViewCell.reuseIdentifier, bundle: nil), forCellWithReuseIdentifier: ImageSearchCollectionViewCell.reuseIdentifier)
+        
+        setCollectionViewUI()
         fetchImage()
-
+        
     }
     
     // 네트워크 통신 시 메서드 이름: fetch or request or callRequest or get ... -> 서버의 response에 따라 네이밍을 설정해주기도 함.
@@ -30,12 +41,47 @@ class ImageSearchViewController: UIViewController {
                 let json = JSON(value)
                 print("JSON: \(json)")
                 
+                for item in json["items"].arrayValue {
+                    let imageURL = item["link"].stringValue
+                    self.imageList.append(imageURL)
+                }
                 
+                print(self.imageList)
+                
+                self.imageSearchCollectionView.reloadData()
                 
             case .failure(let error):
                 print(error)
             }
         }
     }
+}
 
+extension ImageSearchViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func setCollectionViewUI() {
+        let layout = UICollectionViewFlowLayout()
+        let spacing: CGFloat = 8
+        let width = UIScreen.main.bounds.width - ( spacing * 4)
+        let height = UIScreen.main.bounds.height / 4
+        layout.itemSize = CGSize(width: width / 3, height: height)
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        imageSearchCollectionView.collectionViewLayout = layout
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageSearchCollectionViewCell.reuseIdentifier, for: indexPath) as? ImageSearchCollectionViewCell else { return UICollectionViewCell() }
+        
+        cell.searchImageView.kf.setImage(with: URL(string: imageList[indexPath.row]))
+        
+        return cell
+    }
 }
